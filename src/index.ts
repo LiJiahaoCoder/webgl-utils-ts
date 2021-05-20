7e4e7e858b091aec75184c481fae5dd82a77e8a0
Jiahao Li <lijiahao_shandong@163.com>
2021-05-20 17:07:59 +0800

    resize canvas to display size

--- src/index.ts
+++ src/index.ts
@@ -125,3 +125,20 @@ export const createProgramFromSources = (
 
   return createProgram(gl, shaders, attributes, locations);
 };
+
+export const resizeCanvasToDisplaySize = (
+  canvas: HTMLCanvasElement,
+  multiplier: number,
+) => {
+  multiplier = multiplier || 1;
+  const width = canvas.clientWidth * multiplier | 0;
+  const height = canvas.clientHeight * multiplier | 0;
+
+  if (canvas.width !== width || canvas.height !== height) {
+    canvas.width = width;
+    canvas.height = height;
+    return true;
+  }
+
+  return false;
+};
